7d38f2ed51051a7c06adb7a11d25333fbcff6ddb
Ogai Masataka <gaogaomango@gmail.com>
2018-12-29 01:44:24 +1100

    add channel header

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
+import Channels from "./Channels";
 import UserPanel from "./UserPanel";
 
 class SidePanel extends React.Component {
@@ -14,6 +15,7 @@ class SidePanel extends React.Component {
         style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
       >
         <UserPanel currentUser={currentUser} />
+        <Channels />
       </Menu>
     );
   }
