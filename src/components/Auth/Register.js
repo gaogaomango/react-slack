6680aa2014fe5661274a4e78ea1883fa5a89f832
Ogai Masataka <gaogaomango@gmail.com>
2018-12-28 17:59:07 +1100

    register form validation

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,48 +1,84 @@
 import React from 'react';
 import firebase from '../../firebase';
 import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Register extends React.Component {
   state = {
     username: "",
     email: "",
     password: "",
-    passwordConfirmation: ""
+    passwordConfirmation: "",
+    errors: [],
   };
 
+  isFormValid = () => {
+    let errors = [];
+    let error;
+    if (this.isFormEmpty(this.state)) {
+      error = { message: "Fill in all fields" };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else if (!this.isPasswordValid(this.state)) {
+      error = { message: "Password is invalid" };
+      this.setState({
+        errors: errors.concat(error)
+      });
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  isPasswordValid = ({ username, email, password, passwordConfirmation }) => {
+    if (password.length < 6 || passwordConfirmation.length < 6) {
+      return false;
+    } else if (password !== passwordConfirmation) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+    return !username.length || !email.length || !password.length || !passwordConfirmation.length;
+  }
+
+  displayErrors = errors => errors.map((error, i) => <p key={i}>{error.message}</p>);
+
   handleChange = event => {
-    this.setState({[event.target.name]: event.target.value});
+    this.setState({ [event.target.name]: event.target.value });
   };
 
   handleSubmit = event => {
-    event.preventDefault();
-    firebase
-    .auth()
-    .createUserWithEmailAndPassword(this.state.email, this.state.password)
-    .then(createdUser => {
-      console.log(createdUser);
-    })
-    .catch(err => {
-    console.error(err);
-    } 
-    );
+    if (this.isFormValid()) {
+      event.preventDefault();
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(createdUser => {
+          console.log(createdUser);
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
   }
 
   render() {
-    const {username, email, password, passwordConfirmation} = this.state;
+    const { username, email, password, passwordConfirmation, errors } = this.state;
 
     return (
 
       <Grid textAlign="center" verticalAlign="middle" className="app">
-        <Grid.Column style={{maxWidth: 450}}>
+        <Grid.Column style={{ maxWidth: 450 }}>
           <Header as="h2" icon color="orange" textAlign="center">
             <Icon name="puzzle piece" color="orange" />
             Register for DevChat
           </Header>
           <Form onSubmit={this.handleSubmit} size="large">
             <Segment stacked>
-              <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="User Name" onChange={this.handleChange} value={username} type="text"/>
+              <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="User Name" onChange={this.handleChange} value={username} type="text" />
 
               <Form.Input fluid name="email" icon="mail" iconPosition="left" placeholder="Email Address" onChange={this.handleChange} value={email} type="email" />
 
@@ -53,6 +89,12 @@ class Register extends React.Component {
               <Button color="orange">Submit</Button>
             </Segment>
           </Form>
+          {errors.length > 0 && (
+            <Message error>
+              <h3>Error</h3>
+              {this.displayErrors(errors)}
+            </Message>
+          )}
           <Message>Already a user? <Link to="/login">Login</Link> </Message>
         </Grid.Column>
       </Grid>
