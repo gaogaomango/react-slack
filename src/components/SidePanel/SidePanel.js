ec33f131bbb3099fdf1a342b4c7b172d4f22e4db
Ogai Masataka <gaogaomango@gmail.com>
2018-12-30 00:55:23 +1100

    create direct message title

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Menu } from "semantic-ui-react";
 import Channels from "./Channels";
 import UserPanel from "./UserPanel";
+import DirectMessages from "./DirectMessages";
+import { Menu } from "semantic-ui-react";
 
 class SidePanel extends React.Component {
   render() {
@@ -16,6 +17,7 @@ class SidePanel extends React.Component {
       >
         <UserPanel currentUser={currentUser} />
         <Channels currentUser={currentUser} />
+        <DirectMessages />
       </Menu>
     );
   }
