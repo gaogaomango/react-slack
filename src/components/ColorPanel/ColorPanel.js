bb08def4a2253c26cd9740b006d441f040046f8d
Ogai Masataka <gaogaomango@gmail.com>
2019-01-01 21:52:36 +1100

    show setting primary color and secondary color

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -18,7 +18,23 @@ class ColorPanel extends React.Component {
     primary: "",
     secondary: "",
     user: this.props.currentUser,
-    usersRef: firebase.database().ref("users")
+    usersRef: firebase.database().ref("users"),
+    userColors: []
+  };
+
+  componentDidMount() {
+    if (this.state.user) {
+      this.addListener(this.state.user.uid);
+    }
+  }
+
+  addListener = userId => {
+    let userColors = [];
+    this.state.usersRef.child(`${userId}/colors`).on("child_added", snap => {
+      userColors.unshift(snap.val());
+      console.log(userColors);
+      this.setState({ userColors: userColors });
+    });
   };
 
   handleChangePrimary = color => this.setState({ primary: color.hex });
@@ -46,12 +62,28 @@ class ColorPanel extends React.Component {
       .catch(err => console.log(err));
   };
 
+  displayUserColors = colors =>
+    colors.length > 0 &&
+    colors.map((color, i) => (
+      <React.Fragment key={i}>
+        <Divider />
+        <div className="color__container">
+          <div className="color__square" style={{ background: color.primary }}>
+            <div
+              className="color__overlay"
+              style={{ background: color.secondary }}
+            />
+          </div>
+        </div>
+      </React.Fragment>
+    ));
+
   openModal = () => this.setState({ modal: true });
 
   closeModal = () => this.setState({ modal: false });
 
   render() {
-    const { modal, primary, secondary } = this.state;
+    const { modal, primary, secondary, userColors } = this.state;
 
     return (
       <Sidebar
@@ -64,6 +96,7 @@ class ColorPanel extends React.Component {
       >
         <Divider />
         <Button icon="add" size="small" color="blue" onClick={this.openModal} />
+        {userColors && this.displayUserColors(userColors)}
 
         {/* color picker Modal */}
         <Modal basic open={modal} onClose={this.closeModal}>
